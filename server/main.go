7dfe091664e58248ca84e0164ecc8cfc237f5552
Paul Mauviel <ozzadar@ozzadar.com>
2021-10-18 20:25:37 -0400

    Initial Commit

---
+++ server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H {
+			"message": "pong",
+		})
+	})
+
+	if err := r.Run(); err != nil {
+		fmt.Println("Server stopped unexpectedly!")
+	}
+}
