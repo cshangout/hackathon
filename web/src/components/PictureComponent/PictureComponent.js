99b47827eee477eaa6f04810288e193cf7756402
Paul Mauviel <pmauviel@imvu.com>
2021-11-06 23:05:42 -0400

    Adding picture component

---
+++ web/src/components/PictureComponent/PictureComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PictureComponent(props) {
+    return (
+        <div>
+            I'm a picture!
+        </div>
+    )
+}
+
+export default PictureComponent;
